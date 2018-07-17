d34866a85bfe3cce3f3ad862956c93e5ba24bfb7
Eric Thorley <38231351+Thorleye@users.noreply.github.com>
2018-07-16 22:15:26 -0400

    Add files via upload
    Added group project to my git repository
    Origin seppar93

---
+++ visualizeMe/getData.js
@@ -0,0 +1,367 @@
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////API DATA AND GRAPH/////////////////////////////
+// var foodName;
+// var foodNutrientValue;
+// let someStuff = Math.floor(profileData.trainedOuput.cal) * 100;
+document.getElementById("result").innerHTML = getNueralData(profileData); // result shows the percentage the nureal network returns
+
+
+var nutrientLookup = nutrientSearch()
+function nutrientSearch(){
+    $('#foodSearch').on('click', function(){
+        var APIKey = 'api_key=AChcEWV59WuOEXHT383nb04fkKXXtobx5EhmViQB';
+        var nutrientChoice = 'nutrients=' + $('#nutrient').val()
+        var foodGroup = 'fg=' + $('#foodGroup').val()
+        var queryURL = 'https://api.nal.usda.gov/ndb/nutrients/?format=json&' + APIKey + '&' + nutrientChoice + '&' + foodGroup + '&subset=1&sort=c&max=5'
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+            }).then(function(response) {
+                var nutritionArray = response.report.foods
+                var nutritionNameArray = [];
+                var nutrientValueArray = [];
+                for (i in nutritionArray){
+                    nutritionNameArray.push(nutritionArray[i].name);
+                }
+                for (j = 0; j < nutritionArray.length; j++){
+                    var nutritionInfo = nutritionArray[j].nutrients
+                    for (k in nutritionInfo){
+                        nutrientValueArray.push(nutritionInfo[k].value);
+                    }
+                }
+///////////////////////////////API Graph////////////////////////////////////////
+                  var ctx = document.getElementById("api-bar-graph"); // whatever name we are going to give the calories div
+                  var caloriesChart = new Chart(ctx, {
+                    type: 'bar',  //NOTE: ideally we can make this tied to a dropdown menu on the HTML to change chart type
+                    data: {
+                      labels : nutritionNameArray,  //the variable pulled from the JSON loop
+                      datasets: [ {
+                        label: "Calorie example",
+                        backgroundColor: 'rgb(255, 99, 132)',
+                        borderColor: 'rgb(255, 99, 132)',
+                        data: nutrientValueArray,     //the other variable pulled from the JSON loop
+                      }]
+                    },
+                    options: {
+                      scales: {
+                        yAxes: [{
+                          ticks: {
+                            beginAtZero: true
+                          }
+                        }]
+                      }
+                    }
+                  });
+                });
+              })
+            }
+
+
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////DATA RETREVAL FUNCTION/////////////////////////
+
+function getData(dataset,getFunction) {
+  // if ("data" in dataset) {
+  //   return dataset["data"];
+  // }
+  for(let key in dataset) {
+    if (key === "data") {
+      let info = dataset[key];
+      return getFunction(info);
+    }
+  }
+}
+
+function getNueralData(dataset) {
+  for(let key in dataset) {
+    if (key === "trainedOuput") {
+      return dataset[key].cal * 100;
+    }
+  }
+}
+// console.log(getNueralData(profileData));
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////INPUT FUNCTIONS////////////////////////////////
+
+
+function getCalories(info) {
+  let cal = [];
+  for (var i = 0; i < info.length; i++) {
+    let arraySet = info[i];
+    cal.push(arraySet.calories);
+    // console.log(arraySet.calories);
+  }
+  return cal;
+
+}
+
+function getCarbs(info) {
+  let carbs = [];
+  for (var i = 0; i < info.length; i++) {
+    let arraySet = info[i];
+    carbs.push(arraySet.carbs);
+    // console.log(arraySet.calories);
+  }
+  return carbs;
+
+}
+
+function getFats(info) {
+  let fats = [];
+  for (var i = 0; i < info.length; i++) {
+    let arraySet = info[i];
+    fats.push(arraySet.fat);
+    // console.log(arraySet.calories);
+  }
+  return fats;
+}
+
+function getProtein(info) {
+  let proteins = [];
+  for (var i = 0; i < info.length; i++) {
+    let arraySet = info[i];
+    proteins.push(arraySet.protein);
+    // console.log(arraySet.calories);
+  }
+  return proteins;
+}
+
+function getDate(info) {
+  let date = [];
+  for (var i = 0; i < info.length; i++) {
+    let arraySet = info[i];
+    date.push(arraySet.date);
+  }
+  return date;
+
+}
+// console.log(getData(profileData,getCalories));
+// console.log(getData(profileData,getCarbs));
+// console.log(getData(profileData,getFats));
+// console.log(getData(profileData,getProtein));
+// console.log(getData(profileData,getDate));
+
+function sum(arr) {
+  return arr.reduce((accum, curr) => accum + curr);
+}
+
+// console.log(sum(getData(profileData,getCalories)));
+
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////GRAPHS/////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////STACKED CALORIE BAR GRAPH//////////////////////
+
+var ctx = document.getElementById("stacked-bar-graph");
+ctx.height = 100; // whatever name we are going to give the calories div
+var caloriesChart = new Chart(ctx, {
+type: 'bar',  //NOTE: ideally we can make this tied to a dropdown menu on the HTML to change chart type
+data: {
+  labels : getData(profileData,getDate),
+  datasets: [{
+       label: 'protein',
+       data: getData(profileData,getProtein),
+      backgroundColor: "rgba(240,152,38,0.4)",
+      borderColor: "rgba(240,152,38,1)"
+     },{
+       label: 'carbs',
+      data: getData(profileData,getCarbs),
+       backgroundColor: "rgba(51,38,240,0.4)",
+       borderColor: "rgba(51,51,240,1)",
+     }, {
+       label: 'fat',
+       data: getData(profileData,getFats),
+       backgroundColor: "rgba(38,227,240,0.4)",
+       borderColor: "rgba(38,227,240,1)",
+     }]
+   },
+  options: {
+  scales: {
+    yAxes: [{
+      stacked: true
+    }],
+    xAxes: [{
+      stacked: true
+    }]
+  }
+}
+});
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////MACROS PIE GRAPH///////////////////////////////
+
+var ctx = document.getElementById("macros-pie-bar");
+ctx.height = 100;
+
+var myChart = new Chart(ctx, {
+//type of chart
+type: 'pie',
+data: {
+    labels: ["Proteins", "Carbs", "Fats"],
+    datasets: [{
+        label: 'Weekly Intake',
+        //values for chart to display
+        data: [sum(getData(profileData,getProtein)), sum(getData(profileData,getCarbs)), sum(getData(profileData,getFats))],
+        //colours used for graphing labels above
+        backgroundColor: [
+            'rgba(153,255,51,0.4)',
+            'rgba(255,153,0,0.4)',
+            'rgba(260,200,60,0.4)',
+            'rgba(63, 63, 191,0.4)',
+        ],
+        //border colors used for the labels above
+        borderColor: [
+            'rgba(153,255,51,1)',
+            'rgba(255,153,0, 1)',
+            'rgba(260,200,60, 1)',
+            'rgba(63, 63, 191,1)',
+        ],
+        borderWidth: 1
+    }]
+},
+options: {
+    //if we wanted to add graph lines, we would do so in here.
+}
+});
+
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////CALORIES LINE GRAPH///////////////////////////////
+
+
+var ctx = document.getElementById("calorie-line-graph");
+ctx.height = 100;
+var caloriesChart = new Chart(ctx, {
+    type: 'line', //type of chart
+    data: {
+      labels : getData(profileData,getDate),  //variable for dates
+      datasets: [ {
+        label: "calories",//name of chart
+        backgroundColor: 'rgba(0,250,154,0.4)',
+        borderColor: 'rgba(0,250,154,1)',
+        data: getData(profileData,getCalories) ,     //<--the weight variable pulled from the JSON loop
+    }]
+  },
+});
+
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////CARBS LINE GRAPH///////////////////////////////
+
+var ctx = document.getElementById("calorie-carbs-graph");
+ctx.height = 100;
+var caloriesChart = new Chart(ctx, {
+    type: 'line', //type of chart
+    data: {
+      labels : getData(profileData,getDate),  //variable for dates
+      datasets: [ {
+        label: "carbs",//name of chart
+        backgroundColor: 'rgba(0,250,154,0.4)',
+        borderColor: 'rgba(0,250,154,1)',
+        data: getData(profileData,getCarbs) ,     //<--the weight variable pulled from the JSON loop
+    }]
+  },
+});
+
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////PROTIEN LINE GRAPH/////////////////////////////
+var ctx = document.getElementById("calorie-protein-graph");
+ctx.height = 100;
+
+var caloriesChart = new Chart(ctx, {
+    type: 'line', //type of chart
+    data: {
+      labels : getData(profileData,getDate),  //variable for dates
+      datasets: [ {
+        label: "protien",//name of chart
+        backgroundColor: 'rgba(0,250,154,0.4)',
+        borderColor: 'rgba(0,250,154,1)',
+        data: getData(profileData,getProtein) ,     //<--the weight variable pulled from the JSON loop
+    }]
+  },
+});
+
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////FAT LINE GRAPH/////////////////////////////////
+var ctx = document.getElementById("calorie-fat-graph");
+ctx.height = 100;
+var caloriesChart = new Chart(ctx, {
+    type: 'line', //type of chart
+    data: {
+      labels : getData(profileData,getDate),  //variable for dates
+      datasets: [ {
+        label: "fats",//name of chart
+        backgroundColor: 'rgba(0,250,154,0.4)',
+        borderColor: 'rgba(0,250,154,1)',
+        data: getData(profileData,getFats) ,     //<--the weight variable pulled from the JSON loop
+    }]
+  },
+});
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////recipe API/////////////////////////////////////
+
+var recipeLookup = recipeSearch()
+function recipeSearch(){
+    $('#recipeSearch').on("click", function(){
+        var APIKey = '&app_key=0ada957d3714df14b9602bc7869a6178';
+        var applicationID = '&app_id=683d8f0f';
+        var q = $("#recipeQuery").val();
+        var queryURL = 'https://api.edamam.com/api/food-database/parser?ingr=' + q +APIKey + "&from=0" + applicationID;
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+            }).then(function(response) {
+
+
+           // console.log(response.hints);
+           recipeArray = response.hints;
+            // console.log(recipeArray);
+
+            for (i=0; i < recipeArray.length; i++){
+
+                //var recipeName = recipeArray[i].food.label
+                // console.log(recipeName)  // x axis
+
+                var recipeNutrients = recipeArray[i].food.nutrients // yaxis
+                //console.log(recipeNutrients);
+
+                // document.getElementById("recipeResult").innerHTML = recipeNutrients;
+
+                console.log(recipeNutrients.ENERC_KCAL);
+
+                document.getElementById("recipeResult").innerHTML = Math.floor(recipeNutrients.ENERC_KCAL);
+
+
+                //$().append(recipeName);
+                //$().append(recipeNutrients);
+
+
+                //q.append(recipeName);
+                //q.append(recipeNutrients);
+                //$('#recipeSearch'). append(q);
+
+            }
+        })
+    })
+};
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////jquery-navbar//////////////////////////////////
+
+function apiRequest(array) {
+  let result;
+  let obj = array[0];
+  for (let key in obj) {
+    if (key === "food") {
+      let food = obj[key];
+      let data =  food[nutrient];
+      let result = data[ENERC_KCAL];
+    }
+    return result;
+  }
+}
+
+console.log(apiRequest(recipeNutrients));
---
+++ visualizeMe/index.js
@@ -0,0 +1,68 @@
+const brain = require( 'brain.js'); // use of brain.js libary
+var net = new brain.NeuralNetwork(); // setting up neural network
+
+var mfp = require("mfp"); // setting up variable to use MFP module
+const fs = require('fs'); // set up fire writing module
+
+
+mfp.fetchDateRange('Charoolz', '2018-01-15', '2018-07-01', ['calories', 'protein', 'carbs', 'fat'], function(dataSet){
+  // console.log(data['data']);
+  // console.log(dataSet);
+
+  let calories = getData(dataSet,getCalories)
+  // console.log(dataTrain(calories,2000));
+  net.train(dataTrain(calories,2500)); // calorie limit and the ammount eatin over the years
+  var output = net.run({ calories: 1800}); // calorie limit inputed for the week
+  // console.log(output);
+
+  dataSet.trainedOuput = output; // resule
+
+  fs.writeFileSync("dataset.js",'var profileData='+JSON.stringify(dataSet, null ,2)); // writing javascript file formated
+});
+
+// setTimeout(function () {
+  // console.log(dataSet);
+
+  function getData(dataset,getFunction) { // higher orderd function taking data and function
+    for(let key in dataset) {
+      if (key === "data") {
+        let info = dataset[key];
+        return getFunction(info);
+      }
+    }
+  }
+
+  function getCalories(info) {
+    let cal = [];
+    for (var i = 0; i < info.length; i++) {
+      let arraySet = info[i];
+      cal.push(arraySet.calories);
+      // console.log(arraySet.calories);
+    }
+    return cal;
+
+  }
+
+  function dataTrain(arrayCalories,calorieLevel) { // function takes all calories and the level to look for
+    let whatIneed = [];
+    // console.log(arrayCalories);
+
+    for (var i = 0; i < arrayCalories.length; i++) {
+      let num = arrayCalories[i];
+      let expectedResult = true;
+      if (num > calorieLevel) {
+        expectedResult = false;
+      }
+      let format = {input: {calories: num}, output: { cal: expectedResult } };
+      whatIneed.push(format);
+
+    }
+    return whatIneed;
+
+  }
+
+// },1000);
+
+
+// import * as data from "example.json";
+// console.log(data);
